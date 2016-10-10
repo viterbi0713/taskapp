61e7a17c7aa6fcf4db65c61d9a20ea9412b39389
Toru Yoshihara <viterbi0713@gmail.com>
2016-10-10 23:52:19 +0900

    Initial Push before Kadai

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Toru Yoshihara on 2016/10/07.
+//  Copyright © 2016年 tooru.yoshihara. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
